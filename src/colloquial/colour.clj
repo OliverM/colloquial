711f05739e0c1860b25af052e51084895d683287
Oliver Mooney <oliver.mooney@gmail.com>
2016-05-29 22:52:37 +0100

    Use window dimensions instead of screen size!

--- src/colloquial/colour.clj
+++ src/colloquial/colour.clj
@@ -16,12 +16,12 @@
 (defn draw-1-1-1-01-imp
   "Implementing Generative Design's P1-1-1-01.pde - colour spectrum in a grid"
   []
-  (q/color-mode :hsb (q/screen-width) (q/screen-height) 100) ;; set hue and saturation range to window dimensions, brightness 0 to 100
+  (q/color-mode :hsb (q/width) (q/height) 100) ;; set hue and saturation range to window dimensions, brightness 0 to 100
   (q/no-stroke)
   (let [step-x (+ (q/mouse-x) 2)
         step-y (+ (q/mouse-y) 2)
-        width (q/screen-width)
-        height (q/screen-height)]
+        width (q/width)
+        height (q/height)]
     (loop [grid-y 0]
       (when (< grid-y height)
         (loop [grid-x 0]
@@ -34,12 +34,12 @@
 (defn draw-1-1-1-01-fun
    "Re-implement the colour spectrum example in a more functional way"
    []
-   (q/color-mode :hsb (q/screen-width) (q/screen-height) 100) ;; set hue and saturation range to window dimensions, brightness 0 to 100
+   (q/color-mode :hsb (q/width) (q/height) 100) ;; set hue and saturation range to window dimensions, brightness 0 to 100
    (q/no-stroke)
    (let [step-x (+ (q/mouse-x) 2)
          step-y (+ (q/mouse-y) 2)
-         width (q/screen-width)
-         height (q/screen-height)]
+         width (q/width)
+         height (q/height)]
      (doseq [grid-y (range 0 height step-y) ;; doseq doesn't hold on to head of list
              grid-x (range 0 width step-x)]
        (q/fill grid-x (- height grid-y) 100) ;; set current colour according to rect position and scale set previously
