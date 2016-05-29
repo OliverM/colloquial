a3bf6cfc3a41de80ae2c29fffcfa772fb7c5ae58
Oliver Mooney <oliver.mooney@gmail.com>
2016-05-29 22:17:52 +0100

    Added two implementations of P1.1.1
    - the first is a straight-ish implementation of the imperative version
      in java (it follows the same shape, but threads the updated state
      through the let statement). The loop invariants are updated manually
      in the loop and recur invocations.
    - the second is a more functional approach, using the same let
      scaffolding but with a doseq call pulling the x and y positions from
      range generators
    - both use the same small set of drawing commands (exactly the same as
      it happens)
    - bumped quil to the latest version (2.4.0) and clojure to the latest
      stable (1.8.0)
    - added both to the dynamic namespace for demonstration

--- src/colloquial/dynamic.clj
+++ src/colloquial/dynamic.clj
@@ -18,6 +18,7 @@
         y (q/random (q/height))]
     (q/ellipse x y diam diam)))
 
-;(def draw circle-draw)
-(def draw colloquial.colour/draw-1-0-01)
-
+;; (def draw circle-draw)
+;; (def draw colloquial.colour/draw-1-0-01)
+;; (def draw colloquial.colour/draw-1-1-1-01-imp)
+(def draw colloquial.colour/draw-1-1-1-01-fun)
